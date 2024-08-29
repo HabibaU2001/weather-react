31421a1785e617ecab21a2a5843acc9e07184059
HabibaU2001 <156705727+HabibaU2001@users.noreply.github.com>
2024-08-29 17:05:39 +0100

    Changed design of weather-app

--- src/App.js
+++ src/App.js
@@ -3,35 +3,37 @@ import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="Weather-App">
-      <Weather defaultCity="London" />
+    <div className="hero">
+      <div className="Weather-App">
+        <Weather defaultCity="London" />
 
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/HabibaU2001"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Habiba Uddin
-        </a>
-        , is open-sourced on{" "}
-        <a
-          href="https://github.com/HabibaU2001/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github{" "}
-        </a>
-        and hosted on{" "}
-        <a
-          href="https://weathify-react.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/HabibaU2001"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Habiba Uddin
+          </a>
+          , is open-sourced on{" "}
+          <a
+            href="https://github.com/HabibaU2001/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github{" "}
+          </a>
+          and hosted on{" "}
+          <a
+            href="https://weathify-react.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
